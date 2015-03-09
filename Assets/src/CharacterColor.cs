0f52c0ca5277bc80b245217b1af8dd13c527c3f3
Diphoration <yansimon.martellussier@gmail.com>
2015-03-09 15:04:39 -0400

    Collisions
    Character only collides with the color he's in.
    Faked the disappearance of box while changing colour (until we implement
    it in the shader).
    Tiny example level to show mike the proof of concept.

---
+++ Assets/src/CharacterColor.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterColor : MonoBehaviour {
+
+    bool red = false;
+    bool blue = false;
+    bool green = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Torch")
+		{
+			Torch torch = collider.gameObject.GetComponent("Torch") as Torch;
+
+			if(torch.color == Color.red)
+			{
+                red = true;
+			}
+			if(torch.color == Color.blue)
+			{
+                blue = true;
+			}
+			if(torch.color == Color.green)
+			{
+                green = true;
+			}
+
+            SetCollisionLayer();
+		}
+	}
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Torch")
+        {
+            Torch torch = collider.gameObject.GetComponent("Torch") as Torch;
+
+            if (torch.color == Color.red)
+            {
+                red = false;
+            }
+            if (torch.color == Color.blue)
+            {
+                blue = false;
+            }
+            if (torch.color == Color.green)
+            {
+                green = false;
+            }
+
+            SetCollisionLayer();
+        }
+    }
+
+    void SetCollisionLayer()
+    {
+        if(red && blue && green)
+        {
+            gameObject.layer = 16;
+        }
+        else if (red && blue)
+        {
+            gameObject.layer = 13;
+        }
+        else if (red && green)
+        {
+            gameObject.layer = 14;
+        }
+        else if (blue && green)
+        {
+            gameObject.layer = 15;
+        }
+        else if (red)
+        {
+            gameObject.layer = 8;
+        }
+        else if (blue)
+        {
+            gameObject.layer = 9;
+        }
+        else if (green)
+        {
+            gameObject.layer = 10;
+        }
+    }
+}
--- Assets/src/Lightable.cs
+++ Assets/src/Lightable.cs
@@ -7,28 +7,11 @@ public class Lightable : MonoBehaviour {
 	public bool blue = false;
 	public bool green = false;
 
-	public Material litRed;
-	public Material unlitRed;
-
-	public Material litBlue;
-	public Material unlitBlue;
-
-	public Material litGreen;
-	public Material unlitGreen;
-
 	// Use this for initialization
 	void Start () {
-		/*if(red){
-			gameObject.GetComponent<Renderer>().material = unlitRed;
-		} else if (blue) {
-			gameObject.GetComponent<Renderer>().material = unlitBlue;
-		} else {
-			gameObject.GetComponent<Renderer>().material = unlitGreen;
-		}*/
 	}
 
 	void OnTriggerEnter(Collider collider) {
-		//Debug.Log ("collision");
 		if (collider.gameObject.tag == "Torch")
 		{
 			Torch torch = collider.gameObject.GetComponent("Torch") as Torch;
@@ -58,24 +41,11 @@ public class Lightable : MonoBehaviour {
 	void Illuminate(bool light)
 	{
 		if (light) {
-			/*if(red){
-				gameObject.GetComponent<Renderer>().material = litRed;
-			} else if (blue) {
-				gameObject.GetComponent<Renderer>().material = litBlue;
-			} else {
-				gameObject.GetComponent<Renderer>().material = litGreen;
-			}*/
-
 			GetComponent<Collider>().isTrigger = false;
+            GetComponent<Renderer>().enabled = true;
 		} else {
-			/*if(red){
-				gameObject.GetComponent<Renderer>().material = unlitRed;
-			} else if (blue) {
-				gameObject.GetComponent<Renderer>().material = unlitBlue;
-			} else {
-				gameObject.GetComponent<Renderer>().material = unlitGreen;
-			}*/
 			GetComponent<Collider>().isTrigger = true;
+            GetComponent<Renderer>().enabled = false;
 		}
 	}
 }
