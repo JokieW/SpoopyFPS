6495b973aa04882282024c14fdf5a78593a5834b
JokieW <gulliverjko@gmail.com>
2015-03-23 21:33:00 -0400

    Added a basic script to loop block movements

---
+++ Assets/src/BlockMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMover : MonoBehaviour 
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+    public float cycleTime;
+    public AnimationCurve curve;
+
+    private float _startTime;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float t = (Time.time - _startTime)%cycleTime;
+        transform.position = Vector3.Lerp(pointA, pointB, curve.Evaluate(t / cycleTime));
+	}
+}
